cf9b55552cff2834aa9de2a4ed306ca342cd61c0
Sandro Jr <jr.mblsilva@gmail.com>
2025-08-26 23:08:20 -0300

    sandro-calculadora-media

--- src/index.js
+++ src/index.js
@@ -10,26 +10,26 @@ let notas = [];
 let perguntas = ['digite a primeira nota:', 'digite a segunda nota:', 'digite a terceira nota:,'];
 
 // função para perguntar as notas
-function perguntar(i) {}
+function perguntar(i) {
     if (i < perguntas.length) {
       rl.question(perguntas[i], (resposta) =>{
         let nota = parseFloat(resposta);
 
-    if (isNaN(nota)[ nota < 0, nota > 10]) {
-      console.log("❌ Por favor, digite um número válido entre 0 e 10.");
+    if (isNaN(nota) /nota < 0 / nota > 10) {
+      console.log("digite um número válido entre 0 e 10.");
       perguntar(i); // Repete a pergunta
-    } else{
+    } else {
       notas.push(nota);
       perguntar(i + 1);
     }
      });
      } else {
     calcularMedia();
-  
-     }
+  }
+ }
 // função para calcular a média e mostra resultado
 function calcularMedia() {
-    let media = (notas[1] + notas[2] + notas[3]) / 3;
+    let media = (notas[0] + notas[1] + notas[2]) / 3;
     console.log("Notas:", notas.join(", "));
     console.log("Média:", media.toFixed(2));
 
